5afca77d6acde35c563e6d1200a6f9814a609385
bdon <bdon@users.noreply.github.com>
2023-03-04 07:00:56 +0000

    Deploying to gh-pages from @ bdon/maperture@d6b23768dfc9c16c4dfe4dad9965de0bbeb3f4b2 🚀

---
+++ config/local.js
@@ -0,0 +1,39 @@
+const mapboxGlAccessToken =
+  'pk.eyJ1Ijoic3RhbWVuIiwiYSI6ImNreWM1emM2NjAyNTgyb25kc2o5ZG1iMmoifQ.o2QhFbe03ilcplg9sAhYZQ';
+
+const stylePresets = [
+  {
+    id: 'mapbox-streets',
+    name: 'Mapbox Streets',
+    type: 'mapbox-gl',
+    url: 'mapbox://styles/mapbox/streets-v11',
+  },
+  {
+    id: 'mapbox-outdoors',
+    name: 'Mapbox Outdoors',
+    type: 'mapbox-gl',
+    url: 'mapbox://styles/mapbox/outdoors-v11',
+  },
+  {
+    id: 'mapbox-light',
+    name: 'Mapbox Light',
+    type: 'mapbox-gl',
+    url: 'mapbox://styles/mapbox/light-v10',
+  },
+  {
+    id: 'mapbox-dark',
+    name: 'Mapbox Dark',
+    type: 'mapbox-gl',
+    url: 'mapbox://styles/mapbox/dark-v10',
+  },
+  {
+    id: 'openstreetmap',
+    name: 'OpenStreetMap',
+    type: 'leaflet',
+    url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  },
+];
+
+export { mapboxGlAccessToken, stylePresets };
