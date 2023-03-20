0cda060e06f6ed78d85a7eac643d5410a6c25ed5
bdon <bdon@users.noreply.github.com>
2023-03-20 01:20:18 +0000

    Deploying to gh-pages from @ bdon/maperture@5a46b85a7171335471cfc45ea579ab4eea84a705 🚀

--- config/local.js
+++ config/local.js
@@ -1,38 +1,39 @@
-const mapboxGlAccessToken = '';
-
-// Example of setting up a gazetteer object. This is described in the `README`
-// but these are the options that populate the dropdown in the UI that take you
-// to specific locations or map views. You can add as many as you like here and
-// create groupings (in this example, the only group is called Locations).
-const gazetteer = {
-  Locations: [
-    {
-      'San Francisco, CA': {
-        zoom: 18,
-        center: { lng: -122.4193, lat: 37.7648 },
-      },
-    },
-    {
-      'Washington DC': { zoom: 12, center: { lng: -77.0435, lat: 38.9098 } },
-    },
-  ],
-};
+const mapboxGlAccessToken =
+  'pk.eyJ1Ijoic3RhbWVuIiwiYSI6ImNreWM1emM2NjAyNTgyb25kc2o5ZG1iMmoifQ.o2QhFbe03ilcplg9sAhYZQ';
 
 const stylePresets = [
   {
-    id: 'osm-carto',
-    name: 'OSM Carto',
-    type: 'maplibre-gl',
-    renderer: 'maplibre-gl',
-    url: 'config/osm-carto.json',
+    id: 'mapbox-streets',
+    name: 'Mapbox Streets',
+    type: 'mapbox-gl',
+    url: 'mapbox://styles/mapbox/streets-v11',
   },
   {
-    id: 'protomaps-v2-debug',
-    name: 'Protomaps V2 Debug',
-    type: 'maplibre-gl',
-    renderer: 'maplibre-gl',
-    url: 'config/protomaps-v2.json',
+    id: 'mapbox-outdoors',
+    name: 'Mapbox Outdoors',
+    type: 'mapbox-gl',
+    url: 'mapbox://styles/mapbox/outdoors-v11',
+  },
+  {
+    id: 'mapbox-light',
+    name: 'Mapbox Light',
+    type: 'mapbox-gl',
+    url: 'mapbox://styles/mapbox/light-v10',
+  },
+  {
+    id: 'mapbox-dark',
+    name: 'Mapbox Dark',
+    type: 'mapbox-gl',
+    url: 'mapbox://styles/mapbox/dark-v10',
+  },
+  {
+    id: 'openstreetmap',
+    name: 'OpenStreetMap',
+    type: 'leaflet',
+    url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
   },
 ];
 
-export { gazetteer, mapboxGlAccessToken, stylePresets };
+export { mapboxGlAccessToken, stylePresets };
